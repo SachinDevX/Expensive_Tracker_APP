83f416be3825e99f07f190352f586535693438ae
Sachin Sharma <148564606+SachinDevX@users.noreply.github.com>
2024-11-02 16:27:38 +0530

    Update expense_tile.dart

--- expense_tile.dart
+++ expense_tile.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ExpenseTile extends StatelessWidget {
   final String amount;
   final String name;
   final DateTime dateTime;
-  const ExpenseTile({
+  void Function(BuildContext) ? deleTapped;
+   ExpenseTile({
     super.key,
     required this.amount,
     required this.name,
     required this.dateTime,
+    required this.deleTapped,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(name),
-      subtitle:Text(
-        '${dateTime.day}/${dateTime.month}/${dateTime.year}',
+    return Slidable(
+      endActionPane: ActionPane(
+          motion: const StretchMotion(), 
+          children: [
+            //delete button
+            SlidableAction(
+              onPressed: deleTapped,
+              icon: Icons.delete,
+            backgroundColor: Colors.red,
+            borderRadius: BorderRadius.circular(4.0),)
+          ]),
+      child: ListTile(
+        title: Text(name),
+        subtitle:Text(
+          '${dateTime.day}/${dateTime.month}/${dateTime.year}',
+        ),
+        trailing: Text('\$'+amount)
+          ,
       ),
-      trailing: Text('\$'+amount)
-        ,
     );
   }
 }
